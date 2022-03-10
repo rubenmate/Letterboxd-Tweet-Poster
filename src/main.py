9f588afbf6f1478fd4d8f571a62c0cfacbdb886a
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2022-03-10 19:08:49 +0100

    Format changes

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import json
 import emoji
 from config import create_api
 
+
 def letterboxd_rss():
     films_list = []
     lists = []
@@ -20,49 +21,48 @@ def letterboxd_rss():
             description = e.description
 
             if "/list/" in link:
-                list = {
-                    'title': title,
-                    'link': link,
-                    'published': published
-                }
+                list = {"title": title, "link": link, "published": published}
                 lists.append(list)
             else:
                 film = {
-                    'title': title,
-                    'link': link,
-                    'published': published,
-                    'description': description
+                    "title": title,
+                    "link": link,
+                    "published": published,
+                    "description": description,
                 }
                 films_list.append(film)
 
         return films_list, lists
     except Exception as e:
-        print('The scraping job failed. See exception: ')
+        print("The scraping job failed. See exception: ")
         print(e)
+
 
 firstTime = True
 
 # Edit this with your letterboxd feed
-url = 'https://letterboxd.com/rbnmustdie/rss/'
+url = "https://letterboxd.com/rbnmustdie/rss/"
 
 try:
-    with open('config.json', 'r') as f:
+    with open("config.json", "r") as f:
         config = json.load(f)
     firstTime = config["firstTime"]
 except Exception as e:
-    print (e)
+    print(e)
 
 if firstTime:
     films_raw_data, lists_raw_data = letterboxd_rss()
     last_film = films_raw_data[0]
     next_to_last_film = films_raw_data[1]
-    config = {"firstTime": False,
-    # Edit this with your first tweet ID
-    "last_tweet": 1480663891042611202,
-    "index": 1,
-    "next_to_last_film": next_to_last_film, 
-    "last_film": last_film}
-    with open('config.json', 'w') as f:
+    config = {
+        "firstTime": False,
+        # Edit this with your first tweet ID
+        "last_tweet": 1480663891042611202,
+        "index": 1,
+        "next_to_last_film": next_to_last_film,
+        "last_film": last_film,
+    }
+    with open("config.json", "w") as f:
         json.dump(config, f)
 
 api = create_api()
@@ -73,40 +73,62 @@ last_film = config["last_film"]
 next_to_last_film = config["next_to_last_film"]
 
 while True:
-    print('Starting scraping')
+    print("Starting scraping")
 
     films_raw_data, lists_raw_data = letterboxd_rss()
 
     tweet_string = ""
     scrapped_film = films_raw_data[0]
 
-    if scrapped_film['published'] != last_film['published'] and scrapped_film['published'] != next_to_last_film['published']:
+    if (
+        scrapped_film["published"] != last_film["published"]
+        and scrapped_film["published"] != next_to_last_film["published"]
+    ):
         next_to_last_film = last_film
         last_film = scrapped_film
         emoji_star = emoji.emojize(":star:")
-        star = u"\u2605"
-        tweet_string = str(index) + ".- " + str(scrapped_film['title']).replace(star, emoji_star, -1) +'\n' + str(scrapped_film['link']).replace('(','',-1).replace(')','',-1).replace(',','',-1).replace('\'','',-1) +'\n'
+        star = "\u2605"
+        tweet_string = (
+            str(index)
+            + ".- "
+            + str(scrapped_film["title"]).replace(star, emoji_star, -1)
+            + "\n"
+            + str(scrapped_film["link"])
+            .replace("(", "", -1)
+            .replace(")", "", -1)
+            .replace(",", "", -1)
+            .replace("'", "", -1)
+            + "\n"
+        )
         try:
             last_tweet = api.update_status(tweet_string, last_tweet.id)
             index += 1
             now = datetime.datetime.now()
-            print(now.strftime('%H:%M:%S'), "Posted tweet with id:", last_tweet.id, "| Film:", last_film['title'])
+            print(
+                now.strftime("%H:%M:%S"),
+                "Posted tweet with id:",
+                last_tweet.id,
+                "| Film:",
+                last_film["title"],
+            )
         except Exception as e:
             now = datetime.datetime.now()
-            print(now.strftime('%H:%M:%S'), "Failed posting tweet. See exception.")
+            print(now.strftime("%H:%M:%S"), "Failed posting tweet. See exception.")
             print(e)
     else:
         now = datetime.datetime.now()
-        print(now.strftime('%H:%M:%S'), "Nothing to post")
+        print(now.strftime("%H:%M:%S"), "Nothing to post")
         last_film = films_raw_data[0]
         next_to_last_film = films_raw_data[1]
-    
-    config = {"firstTime": False,
-    "last_tweet": last_tweet.id,
-    "index": index,
-    "next_to_last_film": next_to_last_film, 
-    "last_film": last_film}
-    with open('config.json', 'w') as f:
+
+    config = {
+        "firstTime": False,
+        "last_tweet": last_tweet.id,
+        "index": index,
+        "next_to_last_film": next_to_last_film,
+        "last_film": last_film,
+    }
+    with open("config.json", "w") as f:
         json.dump(config, f)
 
     # Wait 15 minutes between checks
